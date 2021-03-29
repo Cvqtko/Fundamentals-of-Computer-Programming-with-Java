4061075a2e1603bb4ceff83e17abf04e7f36d4a4
Cvqtko Kirilov <cvqtkokirilov@gmail.com>
2021-03-29 22:17:57 +0300

    Exercises

---
+++ Exercise_01/src/Exercise_02.java
@@ -0,0 +1,19 @@
+
+public class Exercise_02 {
+	/*
+	 * System: It is a final class defined in the java.lang package. out: This is an
+	 * instance of PrintStream type, which is a public and static member field of
+	 * the System class. println(): As all instances of PrintStream class have a
+	 * public method println(), hence we can invoke the same on out as well. This is
+	 * an upgraded version of print(). It prints any argument passed to it and adds
+	 * a new line to the output. We can assume that System.out represents the
+	 * Standard Output Stream.
+	 */
+	  public static void main(String[] args)
+	    {
+	        System.out.println("Welcome");
+	        System.out.println("To");
+	        System.out.println("Learning Java");
+	        System.err.println("Error");
+	    }
+}
