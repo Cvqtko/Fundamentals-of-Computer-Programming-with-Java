584fb8c7ad5a10bae02cb4f5cfb6937916ab7375
Cvqtko Kirilov <cvqtkokirilov@gmail.com>
2021-03-31 20:33:50 +0300

    Delete

--- Exercise_02/src/Exercise_04.java
+++
@@ -1,6 +0,0 @@
-// 4. Декларирайте променлива isMale от тип boolean и присвоете стойност на последната в зависимост от вашия пол.
-public class Exercise_04 {
-	public static void main(String[] args) {
-		boolean isMale = true;
-	}
-}
--- Exercise_02/src/Exercise_07.java
+++
@@ -1,7 +0,0 @@
-// 7. Напишете програма, която принтира на конзолата равнобедрен триъгълник, като страните му са очертани от символа звездичка "*".
-public class Exercise_07 {
-	public static void main(String[] args) {
-		String triangle = "    *\n   * *\n  *   *\n *******";
-		System.out.println(triangle);
-	}
-}
