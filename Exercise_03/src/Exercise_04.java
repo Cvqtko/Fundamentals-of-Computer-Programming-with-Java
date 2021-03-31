c95a1fbb45dc423b81ffdbb016677948d6e28b46
Cvqtko Kirilov <cvqtkokirilov@gmail.com>
2021-03-31 20:35:16 +0300

    Delete

--- Exercise_03/src/Exercise_04.java
+++
@@ -1,8 +0,0 @@
-// 4. Ќапишете израз, който да провер€ва дали трети€ бит на дадено число е 1 или 0.
-public class Exercise_04 {
-	public static void main(String[] args) {
-		int number = 8;
-		System.out.println(
-				((number >> 3) & 1) == 1 ? "“рети€ бит на дадено число е 1" : "“рети€ бит на дадено число е 0");
-	}
-}
