f1794d138c2c864421aa926480757ff3cb32c84a
Cvqtko Kirilov <cvqtkokirilov@gmail.com>
2021-05-05 19:16:06 +0300

    Delete

--- Exercise_12/src/Exercise_04.java
+++
@@ -1,6 +0,0 @@
-/* 4. Да се намерят всички стандартни изключения от йерархията на java.lang.IllegalArgumentException.*/
-public class Exercise_04 {
-/*	public class IllegalArgumentException
-	extends RuntimeException
-	Thrown to indicate that a method has been passed an illegal or inappropriate argument.*/
-}
